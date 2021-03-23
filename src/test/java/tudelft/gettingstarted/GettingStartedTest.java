8e0604a3e7bfab81638b50d6207ee23e8a0fb99a
Alexander Kirsch <akirsch@telekom.de>
2021-03-23 13:22:00 +0100

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,11 +1,13 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
 
     @Test
+//    @DisplayName("1.0 - First Test - already complete")
     public void addFiveTo20() {
         int result = new GettingStarted().addFive(20);
         Assertions.assertEquals(25,result);
@@ -13,15 +15,17 @@ public class GettingStartedTest {
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
+    @Test
+    @DisplayName("2.1 - had to include numbers")
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
 //
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    @DisplayName("2.2 - had to include numbers - again")
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals((-15),result);
+    }
 }
